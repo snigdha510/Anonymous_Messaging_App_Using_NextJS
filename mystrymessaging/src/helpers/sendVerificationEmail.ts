d6afeecc4c0e59d5af3477f0c8629564c41cdf37
snigdha510 <sparashar5102001@gmail.com>
2025-01-10 22:30:30 +0530

    Added sending email for verification code files

--- mystrymessaging/src/helpers/sendVerificationEmail.ts
+++ mystrymessaging/src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,26 @@
 import {resend} from "@/lib/resend";
 
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string 
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Verification Code of Mystery Message',
+            react: VerificationEmail({username, otp: verifyCode}),
+          });
+          
+        return {success: false,message: "Verification email sent successfully."}
+    } catch (emailError) {
+        console.error("Error in sending verification email.", 
+            emailError)
+        return {success: false, message: "Failed to send verification email"}
+    }
+}
